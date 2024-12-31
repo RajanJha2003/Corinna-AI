68a0825dbc573e82888c6471466f8a7a8884259f
RajanJha2003 <rajanj448@student.sfit.ac.in>
2024-12-31 13:28:22 +0530

    provider and components

--- src/components/forms/sign-up/form-provider.tsx
+++ src/components/forms/sign-up/form-provider.tsx
@@ -1,8 +1,28 @@
+'use client';
+
+import { Loader } from '@/components/loader';
+import { AuthContextProvider } from '@/context/use-auth-context';
 import React from 'react'
+import {FormProvider} from 'react-hook-form'
 
 const SignUpFormProvider = () => {
   return (
-    <div>SignUpFormProvider</div>
+   <AuthContextProvider>
+    <FormProvider>
+        <form className='h-full'>
+            <div className='flex flex-col justify-between gap-3 h-full'>
+            <Loader>
+                {children}
+            </Loader>
+
+                
+
+            </div>
+
+        </form>
+
+    </FormProvider>
+   </AuthContextProvider>
   )
 }
 
