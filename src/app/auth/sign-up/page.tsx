1e0c63692c17721b67ef26fa112a8a2c5061a474
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-01-01 12:47:20 +0530

    handler sign up

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,4 +1,6 @@
+import ButtonHandler from '@/components/forms/sign-up/button-handlers'
 import SignUpFormProvider from '@/components/forms/sign-up/form-provider'
+import HighLightBar from '@/components/forms/sign-up/highlight-bar'
 import RegistrationFormStep from '@/components/forms/sign-up/registration-step'
 import React from 'react'
 
@@ -9,8 +11,10 @@ const SignUp = () => {
         <SignUpFormProvider>
           <div className='flex flex-col gap-3'>
             <RegistrationFormStep />
+            <ButtonHandler />
 
           </div>
+          <HighLightBar />
           
         </SignUpFormProvider>
       </div>
